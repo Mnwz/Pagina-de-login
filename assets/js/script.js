4dba2b00cf8a673cdcf183dd4a460d95a105db74
Mnwz <marcosorlandork@gmail.com>
2023-03-18 14:44:27 -0300

    Testado e funcional.
    Arrumado o problema do javascript no qual não estava sendo possível criar e deletar uma class no main ao clicar no "inscreva-se" e "entrar".

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 const inputs = document.querySelectorAll(".input-field");
 const toggle_btn = document.querySelectorAll(".toggle");
-const main = document.querySelectorAll("main");
+const main = document.querySelector("main");
 
 inputs.forEach((inp) => {
-    inp.addEventListener("focus", () => {
-      inp.classList.add("active");
-    });
-    inp.addEventListener("blur", () => {
-      if (inp.value != "") return;
-      inp.classList.remove("active");
-    });
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
   });
-  
-  toggle_btn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      main.classList.toggle("sign-up-mode");
-    });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
   });
+});
+
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("sign-up-mode");
+  });
+});
